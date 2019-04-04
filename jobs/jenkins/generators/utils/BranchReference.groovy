7a903b8c0a8cfba84becd97e58553a8a4246a9ff
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-04-04 21:43:31 +1300

    More seeing if this loads

--- jobs/jenkins/generators/utils/BranchReference.groovy
+++ jobs/jenkins/generators/utils/BranchReference.groovy
@@ -1,22 +1,6 @@
 package jenkins.generators.utils
 
-public class BranchReference {
-    private String Id
-    private String Refspec
-
-    String getId() {
-        return this.Id
-    }
-
-    String getRefspec() {
-        return this.Refspec
-    }
-
-    void setId(String id) {
-        this.Id = id
-    }
-
-    void setRefspec(String refspec) {
-        this.Refspec = refspec
-    }
+class BranchReference {
+    String id
+    String refspec
 }
--- jobs/jenkins/generators/utils/Project.groovy
+++ jobs/jenkins/generators/utils/Project.groovy
@@ -3,41 +3,8 @@ package jenkins.generators.utils;
 import java.util.*;
 
 class Project {
-
-    String Area;
-    String Description;
-    String Name;
-    List<BranchReference> Refspecs;
-
-    String getArea() {
-        return this.Area;
-    }
-
-    String getDescription() {
-        return this.Description;
-    }
-
-    String getName() {
-        return this.Name;
-    }
-
-    List<BranchReference> getRefspecs() {
-        return this.Refspecs;
-    }
-
-    void setArea(String area) {
-        this.Area = area;
-    }
-
-    void setDescription(String description) {
-        this.Description = description;
-    }
-
-    void setName(String name) {
-        this.Name = name;
-    }
-
-    void setRefspecs(List<BranchReference> refspecs) {
-        this.Refspecs = refspecs;
-    }
+    String area;
+    String name;
+    String url;
+    //List<BranchReference> Refspecs;
 }
